5fcda97715669aa7d32e76c853579b2f5a7acac6
Alex Paredes <alexparedes83@gmail.com>
2023-03-30 08:30:18 -0600

    firebase connected to app and is saving new data

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// export const environment: any = {
+//     production: false,
+//     firebase: {
+//       apiKey: 'AIzaSyBfkvoNLakSVR2TqSjTn7n9gVAFb2Mu6x8',
+//       authDomain: 'hr-timesheet-8bb18.firebaseapp.com',
+//       databaseURL: 'https://hr-timesheet-8bb18-default-rtdb.firebaseio.com',
+//       projectId: 'hr-timesheet-8bb18',
+//       storageBucket: 'hr-timesheet-8bb18.appspot.com',
+//       messagingSenderId: '149700857715',
+//       appId: '1:149700857715:web:971542c52b2d09e2303f06',
+//       measurementId: 'G-B5QMKZ7Z4T'
+//     },
+//   };
